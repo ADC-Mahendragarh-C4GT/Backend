bee4abe54da26f52821e974189961acce47b9a72
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-06-28 16:01:17 +0530

    Logout functionality backend added

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.contrib.auth import login
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .serializers import *
+from django.contrib.auth import logout
 
 class RegisterView(APIView):
     def post(self, request):
@@ -29,3 +30,10 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
